3eb7ab3363f0b689d7c70339958fc57fdb9bc33a
DanielFigueroa1 <daniel.alejandroolmedo@outlook.es>
2020-10-29 10:56:44 -0500

    nuevo

--- components/FilaPost.js
+++ components/FilaPost.js
@@ -16,6 +16,10 @@ class FilaPost {
         nombreFeed.className = "nombreFeed";
         nombreFeed.innerHTML = this.publicacionUsuario.usuario;
 
+        let divComentario = document.createElement('div'); //comentarios
+        divComentario.className = "comentarios";
+        //divComentario.innerHTML = this.publicacionUsuario.Comentario;
+
         let inputResponder = document.createElement('input'); //input responder
         inputResponder.className = "inputResponderPost";
         inputResponder.innerHTML = '';
@@ -24,12 +28,38 @@ class FilaPost {
         botonResponder.className = "botonResponderPost";
         botonResponder.innerHTML = 'Responder';
 
+        let divMensaje = document.createElement('div');
+
+        let database = firebase.database();
+        botonResponder.addEventListener('click', ()=>{
+            //alert(this.publicacionUsuario.usuario);
+            let Comentario = {
+                comentario: inputResponder.value
+            }
+            
+            database.ref('usuarios/blog/'+this.publicacionUsuario.id+'/comentarios').push().set(Comentario);
+            
+            divMensaje.className = "mensajesRespuetas";
+            divMensaje.innerHTML = Comentario.comentario;
+            divComentario.appendChild(divMensaje);
+        });
+
+        /*database.ref('usuarios/blog/'+this.publicacionUsuario.id+'/comentarios').on('value', function (Comentario){
+            Comentario.forEach(Comentario => {
+            divMensaje.innerHTML = Comentario.comentario;
+            divComentario.appendChild(divMensaje);
+            });
+        });*/
+            
+        
         
         //para que se dibujen
         component.appendChild(postFeed);
         component.appendChild(nombreFeed);
+        component.appendChild(divComentario);
         component.appendChild(inputResponder);
         component.appendChild(botonResponder);
+        
 
         return component;
     }
